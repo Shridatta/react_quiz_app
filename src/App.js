35080ec83ec9ef0eef2d8cfbefc41d0443ec8372
Shridatta <sgz2@njit.edu>
2018-12-05 16:47:28 -0500

    added material-ui navbar,card and buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import Quiz from "./components/Quiz";
 import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 
 class App extends Component {
   constructor() {
@@ -13,23 +21,34 @@ class App extends Component {
   showIntro() {
     return (
       <div>
-        <nav className="navbar navbar-light bg-light">
-          <span className="navbar-brand mb-0 h1">React Quiz</span>
-        </nav>
-        <div className="content">
-          <p>Click Begin to Take the React Quiz !!!</p>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu" />
+            <Typography variant="h6" color="inherit">
+              React Quiz App
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-          <div>
-            <button
-              className="btn btn-primary"
+        <Card>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              Click Begin to Take the React Quiz !!!
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              variant="contained"
+              color="primary"
               onClick={() => {
                 this.start();
               }}
             >
               Begin
-            </button>
-          </div>
-        </div>
+            </Button>
+          </CardActions>
+        </Card>
       </div>
     );
   }
